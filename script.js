b6dd4cadcfb546c19f9da7de81ca58cffca96cd9
Cameron <c@cameronfrost.com>
2021-01-28 12:09:19 -0600

    Query selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("#new-todo-form");
+const todoInput = document.querySelector("#todo-input");
+const list = document.querySelector("#list");
+const template = document.querySelector("#list-item-template");
